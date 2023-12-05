76d80337c7ecf1fe020846aad353549fcf508c2b
Osmani Leon <osmanileon92@gmail.com>
2023-12-05 15:59:57 -0600

    Project initial backend setup

---
+++ backend/src/urls.py
@@ -0,0 +1,10 @@
+# backend/src/urls.py
+
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+   
+    # Add other URLs for your project
+]
